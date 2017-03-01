7a817efe910b01d65ae2e0dc078b966bc641c72b
Walter Brown <zinecarton@gmail.com>
2017-03-01 12:54:07 -0800

    Made it easier to recognize PASSes and FAILures in main

--- src/cache/main.cpp
+++ src/cache/main.cpp
@@ -15,7 +15,7 @@ int main() {
            << "result[2]=" << result->at(2) << endl;
     }
     else {
-      cout << "Split address looks good so far..." << endl;
+      cout << "PASS" << endl;
     }
     result = l1Cache.splitAddress(31); // put 0s in tag spot.
     if(result->at(0) != 0) {
@@ -24,7 +24,7 @@ int main() {
            << "result[2]=" << result->at(2) << endl;
     }
     else {
-      cout << "Split address looks swell!" << endl;
+      cout << "PASS" << endl;
     }
     cout << "END splitAddress tests" << endl;
   }
@@ -34,10 +34,10 @@ int main() {
     vector<int> *split = l1Cache.splitAddress(1023);
     int result = l1Cache.buildAddress(split->at(0), split->at(1), split->at(2));
     if(result == 1023) {
-      cout << "good..." << endl;
+      cout << "PASS" << endl;
     }
     else {
-      cout << "BAD: " << result << endl;
+      cout << "FAIL: " << result << endl;
     }
     cout << "END buildAddress tests" << endl;
   }
@@ -47,35 +47,35 @@ int main() {
     // Make sure empty at start.
     int result = l1Cache.addressWay(4294967295);
     if(result == -1) {
-      cout << "good so far..." << endl;
+      cout << "PASS" << endl;
     }
     else {
-      cout << "RETURNED " << result << " on empty!" << endl;
+      cout << "FAIL: RETURNED " << result << " on empty!" << endl;
     }
     // Still nothing is valid, even if the tag exists.
     result = l1Cache.addressWay(0);
     if(result == -1) {
-      cout << "good so far..." << endl;
+      cout << "PASS" << endl;
     }
     else {
-      cout << "RETURNED " << result << " on invalid but good tag" << endl;
+      cout << "FAIL: RETURNED " << result << " on invalid but good tag" << endl;
     }
     // Now we're valid, but bad tag.
     l1Cache.valid->at(0)->at(2) = 1;
     result = l1Cache.addressWay(4294967295);
     if(result == -1) {
-      cout << "good so far..." << endl;
+      cout << "PASS" << endl;
     }
     else {
-      cout << "RETURNED " << result << " on valid but bad tag!" << endl;
+      cout << "FAIL: RETURNED " << result << " on valid but bad tag!" << endl;
     }
     // Valid and good tag should do good things.
     result = l1Cache.addressWay(0);
     if(result == 2) {
-      cout << "good so far..." << endl;
+      cout << "PASS" << endl;
     }
     else {
-      cout << "RETURNED " << result << " on valid and good tag!" << endl;
+      cout << "FAIL: RETURNED " << result << " on valid and good tag!" << endl;
     }
     cout << "END of addressWay test!" << endl;
   }
@@ -83,10 +83,10 @@ int main() {
     cout << "Testing updateLRU" << endl;
     Cache l1Cache(5,0,0, 2, 2, NULL, NULL);
     if(l1Cache.getLRUWay(0) == 0) {
-      cout << "getLRUWay Looks good." << endl;
+      cout << "PASS" << endl;
     }
     else {
-      cout << "BAD: getLRUWay = " << l1Cache.getLRUWay(0) << endl;
+      cout << "FAIL: getLRUWay = " << l1Cache.getLRUWay(0) << endl;
     }
     l1Cache.valid->at(0)->at(0) = 1;
     l1Cache.valid->at(0)->at(1) = 1;
@@ -97,37 +97,37 @@ int main() {
     }
     l1Cache.updateLRU(0);
     if(l1Cache.getLRUWay(0) == 1) {
-      cout << "getLRUWay Looks good." << endl;
+      cout << "PASS" << endl;
     }
     else {
-      cout << "BAD: getLRUWay = " << l1Cache.getLRUWay(0) << endl;
+      cout << "FAIL: getLRUWay = " << l1Cache.getLRUWay(0) << endl;
     }
     l1Cache.updateLRU(1);
     l1Cache.updateLRU(2);
     if(l1Cache.getLRUWay(0) == 3) {
-      cout << "getLRUWay Looks good." << endl;
+      cout << "PASS" << endl;
     }
     else {
-      cout << "BAD: getLRUWay = " << l1Cache.getLRUWay(0) << endl;
+      cout << "FAIL: getLRUWay = " << l1Cache.getLRUWay(0) << endl;
     }
     l1Cache.updateLRU(3);
     l1Cache.updateLRU(3);
     l1Cache.updateLRU(3);
     l1Cache.updateLRU(3);
     if(l1Cache.getLRUWay(0) == 0) {
-      cout << "getLRUWay Looks good." << endl;
+      cout << "PASS" << endl;
     }
     else {
-      cout << "BAD: getLRUWay = " << l1Cache.getLRUWay(0) << endl;
+      cout << "FAIL: getLRUWay = " << l1Cache.getLRUWay(0) << endl;
     }
     if(l1Cache.LRU->at(0)->at(0) == 3 
      &&l1Cache.LRU->at(0)->at(1) == 2 
      &&l1Cache.LRU->at(0)->at(2) == 1 
      &&l1Cache.LRU->at(0)->at(3) == 0) {
-      cout << "Looks good." << endl;
+      cout << "PASS" << endl;
     }
     else {
-      cout << "BAD:";
+      cout << "FAIL:";
       for(int i = 0; i < 4; i++) {
         cout << " LRU[" << i << "]=" << l1Cache.LRU->at(0)->at(i);
       }
@@ -140,10 +140,10 @@ int main() {
      &&l1Cache.LRU->at(0)->at(1) == 3 
      &&l1Cache.LRU->at(0)->at(2) == 2 
      &&l1Cache.LRU->at(0)->at(3) == 1) {
-      cout << "Still Looks good." << endl;
+      cout << "PASS" << endl;
     }
     else {
-      cout << "BAD:";
+      cout << "FAIL:";
       for(int i = 0; i < 4; i++) {
         cout << " LRU[" << i << "]=" << l1Cache.LRU->at(0)->at(i);
       }
@@ -162,13 +162,13 @@ int main() {
     l1Cache.write(1234, 22);
     CacheResult *muffins = l1Cache.read(22);
     if(!muffins) {
-      cout << "l1Cache.read returned a null!!!" << endl;
+      cout << "FAIL: l1Cache.read returned a null!!!" << endl;
     }
     else if(muffins->at(0) != 1234) {
-      cout << "l1Cache.read returned " << muffins->at(0) << endl;
+      cout << "FAIL: l1Cache.read returned " << muffins->at(0) << endl;
     }
     else {
-      cout << "Can write and read single value to cache" << endl;
+      cout << "PASS: Can write and read single value to cache" << endl;
     }
     // Now let's see if I can read and write vectors.
     vector<int> *testVec = new vector<int>();
@@ -185,23 +185,23 @@ int main() {
       }
     }
     if(broke) {
-      cout << "Failed with vector read/write: ";
+      cout << "FAIL: Failed with vector read/write: ";
       for(int i = 0; i < 32; i++) {
         cout << "vec[" << i << "]=" << muffins->at(i) << " ";
       }
       cout << endl;
     }
     else {
-      cout << "Can write and read vector to cache" << endl;
+      cout << "PASS: Can write and read vector to cache" << endl;
     }
     // Now make sure vector realism safeties in place.
     l1Cache.write(new vector<int>(64, 42), 22);
     muffins = l1Cache.read(22, 64);
     if(muffins->size() < 64) {
-      cout << "Safety cropping works!" << endl;
+      cout << "PASS: Safety cropping works!" << endl;
     }
     else {
-      cout << "BAD:  returned vec of size " << muffins->size() << endl;
+      cout << "FAIL:  returned vec of size " << muffins->size() << endl;
     }
     // Make sure you can read through layers of cache.
     testVec = new vector<int>();
@@ -218,14 +218,14 @@ int main() {
       }
     }
     if(broke) {
-      cout << "Failed to recover vector from l3: ";
+      cout << "FAIL: Failed to recover vector from l3: ";
       for(int i = 0; i < 32; i++) {
         cout << "vec[" << i << "]=" << muffins->at(i) << " ";
       }
       cout << endl;
     }
     else {
-      cout << "Can read vector from l3" << endl;
+      cout << "PASS: Can read vector from l3" << endl;
     }
     // Now push modified versions of these values down the caches.
     vector<int> *modifiedVec = new vector<int>();
@@ -265,14 +265,14 @@ int main() {
       }
     }
     if(broke) {
-      cout << "Failed to recover modified vector: ";
+      cout << "FAIL: Failed to recover modified vector: ";
       for(int i = 0; i < 32; i++) {
         cout << "vec[" << i << "]=" << muffins->at(i) << " ";
       }
       cout << endl;
     }
     else {
-      cout << "Can recover modified vector" << endl;
+      cout << "PASS: Can recover modified vector" << endl;
     }
     cout << "END of basic read write tests" << endl;
   }
@@ -296,13 +296,13 @@ int main() {
       }
     }
     if(broke) {
-      cout << "Failed to recover l1: ";
+      cout << "FAIL: Failed to recover l1: ";
       for(int i = 0; i < 32; i++) {
         cout << i << muffins1->at(i) << " " << muffins2->at(i) << endl;
       }
     }
     else {
-      cout << "Can recover l1" << endl;
+      cout << "PASS: Can recover l1" << endl;
     }
     cout << "END save restore tests" << endl;
   }
