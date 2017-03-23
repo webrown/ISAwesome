77e9b64e2907eed5aee655a3a8b8f68baaf45deb
Walter Brown <zinecarton@gmail.com>
2017-03-23 14:01:13 -0700

    Rushing Banana out the door

--- src/pipeline/Banana.h
+++ src/pipeline/Banana.h
@@ -1,13 +1,17 @@
 #ifndef BANANA_H
 #define BANANA_H
+
 #include "../memory/Register.h"
 #include "../memory/MemoryStructure.h"
+#include "../memory/MemoryStructure.h"
+#include "../computer/Status.h"
+
 class Banana {
     public:
         Banana(Register *registers, MemoryStructure *memory);
         ~Banana();
         virtual void init(void);
-        virtual void run(void);
+        virtual Status run(void);
         virtual void stop(void);
 }
 #endif
