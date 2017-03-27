1983682386f05834d66660a1eb60213a1d084092
Walter Brown <zinecarton@gmail.com>
2017-03-26 22:26:42 -0700

    Corrected bug in movement from byte to word addressing in LOD/STO

--- src/pipeline/LoadOperation.cpp
+++ src/pipeline/LoadOperation.cpp
@@ -5,15 +5,20 @@ LoadOperation LoadOperation::singleton;
 
 void LoadOperation::memoryOperation(Register *registers, MemoryStructure *memory, unsigned int address, unsigned int registerIndex) {
   // Decide if you want a vector or a scalar and get 
-  int dataRequested = Register::isVectorIndex(registerIndex)?VECTOR_SIZE:1;
+  int dataRequested = Register::isVectorIndex(registerIndex)?VECTOR_SIZE*4:1;
   // Fetch from memory.
   QueryResult *qr = memory->getDataAccess()->read(address, dataRequested);
+  // Grab every 4.
+  QVector<Value> filteredResult;
+  for(int i = 0; i < qr->result.size(); i+=4) {
+    filteredResult.push_back(qr->result.at(i));
+  }
   // Place the value from memory into the register specified.
   if(dataRequested == 1) {
     registers->write(qr->result.at(0), registerIndex);
   }
   else {
-    registers->write(qr->result, registerIndex);
+    registers->write(filteredResult, registerIndex);
   }
   // Cleanup.
   delete qr;
