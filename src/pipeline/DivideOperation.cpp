9aa0a83e92d9b0646cbbb05d1f31a4da988f0b81
Walter Brown <zinecarton@gmail.com>
2017-03-30 12:21:02 -0700

    Added / zero case, also forgot to swap args before.

--- src/pipeline/DivideOperation.cpp
+++ src/pipeline/DivideOperation.cpp
@@ -1,17 +1,48 @@
 #include "DivideOperation.h"
 #include <QDebug>
+#include <limits>
 
 DivideOperation DivideOperation ::singleton;
 
 Value DivideOperation::scalarOperation(int a, int b){
   Value v;
-  v.i = a / b;
+  if(a == 0) {
+    // /0 is just big number.
+    if(b > 0) {
+      v.i = 2147483647;
+    }
+    if(b == 0) {
+      v.i = 0;
+    }
+    if(b < 0) {
+      v.i = -2147483648;
+    }
+  }
+  else {
+    // Save division.
+    v.i = b / a;
+  }
   return v;
 }
 
 Value DivideOperation::scalarOperation(float a, float b){
   Value v;
-  v.f = a / b;
+  if(a == 0) {
+    // /0 is just big number.
+    if(b > 0) {
+      v.f = std::numeric_limits<float>::infinity;
+    }
+    if(b == 0) {
+      v.f = 0;
+    }
+    if(b < 0) {
+      v.f = -std::numeric_limits<float>::infinity;
+    }
+  }
+  else {
+    // Save division.
+    v.f = b / a;
+  }
   return v;
 }
 
